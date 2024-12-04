5d64397acec380606995f42d7728d12d9855c2a1
Tamikaya <tamikaya111@gmail.com>
2024-12-04 13:39:59 +0500

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,19 @@
+alls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.lower() in [any.lower() for any in list_to_search]
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
